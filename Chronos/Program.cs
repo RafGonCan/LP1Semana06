a2efb37c67576cf5bc179ee8de701067932cc5e1
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-27 20:39:07 +0000

    Add instances'crono1' and 'crono2'

---
+++ Chronos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+        }
+    }
+}
