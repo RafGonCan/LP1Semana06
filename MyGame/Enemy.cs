4d067edaa370618a69b272aa2eaf296e44b92b4d
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-27 21:24:08 +0000

    Add class Enemy

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -1,7 +1,18 @@
+using System.Runtime.CompilerServices;
+
 namespace MyGame   
 {
     public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+
+        public Enemy(string name)
+        {
+            this.name = name;
+            health = 100;
+            shield = 0;
+        }
     }
 }
