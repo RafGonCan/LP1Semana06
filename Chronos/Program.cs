fdffee441700f0ea10546b1736b04b25217bad05
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-27 20:40:49 +0000

    Add the countdown and pause

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -10,6 +10,16 @@ namespace Chronos
         {
             Stopwatch crono1 = new Stopwatch();
             Stopwatch crono2 = new Stopwatch();
+            
+            crono1.Start();
+            Thread.Sleep(500);
+            crono2.Start();
+            Thread.Sleep(250);
+            crono1.Stop();
+            crono2.Stop();
+
+            Console.WriteLine("{0:f4}", crono1.ElapsedMilliseconds);
+            Console.WriteLine("{0:f4}", crono2.ElapsedMilliseconds);
         }
     }
 }
