1039f51e79bf10d5e31e94ca3ef942a7c18d2ed0
Rafael Canhoto <rafgoncan99@gmail.com>
2025-03-27 21:01:56 +0000

    Add variables

---
+++ RandDice/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RandDice
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(args[0]);
+            int s = Convert.ToInt32(args[1]);
+
+            }
+        }
+    }
+}
